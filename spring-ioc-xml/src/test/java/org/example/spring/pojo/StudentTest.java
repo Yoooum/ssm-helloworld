f56e69c1bd0a74ddb643c8bf2ab0e9f57d49bb95
Yoooum <akyomi@qq.com>
2022-10-25 16:28:18 +0800

    .

--- spring-ioc-xml/src/test/java/org/example/spring/pojo/StudentTest.java
+++ spring-ioc-xml/src/test/java/org/example/spring/pojo/StudentTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @author 未確認の庭師
  */
